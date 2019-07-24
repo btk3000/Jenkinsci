e3b335f40e089b95691618b3b70cfed18cc7edb2
Ilia Evseenkov <btk3000@gmail.com>
2019-07-24 11:40:07 +0300

    Create SetJobOwner.groovy
    Initial commit

---
+++ groovy/SetJobOwner.groovy
@@ -0,0 +1,13 @@
+import jenkins.model.*;
+import hudson.model.Cause;
+import com.synopsys.arc.jenkins.plugins.ownership.jobs.*;
+
+/*
+Run this scrip as "System Groovy script of Freestyle project.
+build is predefined parameter of Groovy plugin
+For scrip console "import hudson.model.AbstractBuild"
+*/
+
+def startedUser = build.getCause(Cause.UserIdCause).getUserId();
+OwnershipDescription jobOwner = new OwnershipDescription(true,startedUser);
+JobOwnerHelper.setOwnership(Jenkins.getInstance().getItemByFullName(jobName),jobOwner);
